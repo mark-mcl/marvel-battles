e458fa1ebe24f23a0e882cf6d0c42fa07d44b744
Mark McLoughlin <mark.mcloughlin1@gmail.com>
2016-09-30 11:27:48 -0500

    add ajax to voting / character loading

--- sinatra-skeleton/app/controllers/votes.rb
+++ sinatra-skeleton/app/controllers/votes.rb
@@ -1,6 +1,17 @@
 post '/votes' do
   @vote = Vote.create(winner_id: params[:winner_id], loser_id: params[:loser_id])
-  redirect '/'
+  if request.xhr?
+  	character_1 = Character.find(rand(1..72))
+  	character_2 = character_1
+    until character_2 != character_1
+      character_2 = Character.find(rand(1..72))
+    end
+  	content_type :json
+  	{character_1: (erb :'/characters/_show', layout: false, locals: {character: character_1, other_character: character_2}),
+  		character_2: (erb :'/characters/_show', layout: false, locals: {character: character_2, other_character: character_1})}.to_json
+  else
+  	redirect '/'
+  end
 end
 
 get '/votes/index' do
--- sinatra-skeleton/public/js/application.js
+++ sinatra-skeleton/public/js/application.js
@@ -1,7 +1,20 @@
 $(document).ready(function() {
-  // This is called after the document has loaded in its entirety
-  // This guarantees that any elements we bind to will exist on the page
-  // when we try to bind to them
+	$('.container').on('submit', '.vote-form', function(event){
+		console.log('here');
+		event.preventDefault();
+		var $form = $(this)
+		$.ajax({
+			url: $form.attr('action'),
+			method: $form.attr('method'),
+			data: $form.serialize(),
+			dataType: 'json',
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+		})
+		.done(function(response){
+			$('#char1').html(response.character_1);
+			$('#char2').html(response.character_2);
+
+		})
+	})
+  
 });
